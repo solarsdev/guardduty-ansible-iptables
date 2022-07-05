372dfba67b88c1bb842be4dec2690b6253d6b5f1
Sangkook Lee <sangkook.lee@solarsdev.com>
2022-07-05 21:53:27 +0900

    feat: Add SNS, lambda function (send command result to slack)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -42,3 +42,39 @@ resource "aws_lambda_function" "guardduty-findings-trigger" {
     }
   }
 }
+
+//
+// notify-command-result-to-slack
+//
+
+data "archive_file" "notify-command-result-to-slack-src" {
+  type        = "zip"
+  source_dir  = "../code/build/src/notifyCommandResultToSlack"
+  output_path = "../code/output/notifyCommandResultToSlack.zip"
+}
+
+resource "aws_lambda_function" "notify-command-result-to-slack" {
+  filename      = "../code/output/notifyCommandResultToSlack.zip"
+  function_name = "${var.service_name_lowercase}_notify-command-result-to-slack"
+  role          = aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  layers        = [aws_lambda_layer_version.nodejs-common.arn]
+  runtime       = "nodejs14.x"
+
+  source_code_hash = filebase64sha256("../code/output/notifyCommandResultToSlack.zip")
+
+  environment {
+    variables = {
+      TZ                = "Asia/Tokyo"
+      SLACK_WEBHOOK_URL = var.SLACK_WEBHOOK_URL
+    }
+  }
+}
+
+resource "aws_lambda_permission" "default" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notify-command-result-to-slack.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.command-result.arn
+}
--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,1 +1,13 @@
+resource "aws_sns_topic" "command-result" {
+  name = "${var.service_name_lowercase}-command-result-sns"
 
+  tags = merge(var.default_tags, {
+    Name = "${var.service_name_lowercase}.command-result.sns"
+  })
+}
+
+resource "aws_sns_topic_subscription" "command-result-to-slack" {
+  topic_arn = aws_sns_topic.command-result.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.notify-command-result-to-slack.arn
+}
